6299ba4515dca6e3a34a815303a8f8c908128536
Giovanni <giovanni.ggoncalves@outlook.com>
2025-04-15 20:39:47 -0300

    feat: create creators card component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -22,6 +22,29 @@ export default function Home() {
     { id: 6, name: "Esportes", icon: "⚽", count: 195 },
   ];
 
+  const topCreators = [
+    {
+      id: 1,
+      name: "MemeQueen",
+      avatar: "https://i.pravatar.cc/150?img=23",
+      followers: "245K",
+      bio: "Criando memes que fazem seu dia melhor!",
+    },
+    {
+      id: 2,
+      name: "FunnyGuy42",
+      avatar: "https://i.pravatar.cc/150?img=24",
+      followers: "189K",
+      bio: "Especialista em memes de programação e gatos",
+    },
+    {
+      id: 3,
+      name: "LaughFactory",
+      avatar: "https://i.pravatar.cc/150?img=25",
+      followers: "327K",
+      bio: "Se não te fizer rir, devolvo seu tempo!",
+    },
+  ];
 
   
 
